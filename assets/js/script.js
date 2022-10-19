924386efa2071ebf1b267cc8ae52cbdb11bedfce
Alfrew <filigheddualfredo@gmail.com>
2022-10-19 22:42:47 +0200

    feat: carosello referenze

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let activeIndex = 0;
+const slides = document.getElementsByClassName("slide");
+function changeSlide(slideIndex) {
+  slides[activeIndex].classList.remove("active");
+  activeIndex += slideIndex;
+  if (activeIndex === slides.length) {
+    activeIndex = 0;
+  } else if (activeIndex < 0) {
+    activeIndex = slides.length - 1;
+  }
+  setTimeout(() => slides[activeIndex].classList.add("active"), 500);
+  console.log(activeIndex);
+}
