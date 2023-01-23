784cf2c6b79acd68423868f562d36535d416377b
Alfredo.Filigheddu <Alfredo Filigheddu@abirk.it>
2023-01-23 14:05:58 +0100

    feat: accordions

---
+++ assets/js/accordion.js
@@ -0,0 +1,21 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+console.log(acc.item(acc[1]));
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      for (i = 0; i < acc.length; i++) {
+        if (acc[i] !== this) {
+          acc[i].classList.remove("active");
+          acc[i].nextElementSibling.style.maxHeight = null;
+        }
+      }
+    }
+  });
+}
